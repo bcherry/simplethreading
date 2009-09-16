7114bd16cd09e5d8fd7581b3688a666eca0995ba
Ben Cherry <bcherry@gmail.com>
2009-09-15 17:55:34 -0700

    reworked demos page

--- simplethreading.js
+++ simplethreading.js
@@ -1,55 +1,62 @@
-var SimpleThread = function(workFn,params) {
-	var that = this;
-	var autoStart = (params && params.autoStart !== null) || true;
-	var batchSize = (params && params.batchSize) || 5;
-	var interval = (params && params.interval) || 1;
-	var workArgs = (params && params.workArgs) || [];
-	var workFn = workFn;
-	var threadID = null;
+(function(){
+	this.SimpleThread = function(workFn,params) {
+		var that = this;
+		var autoStart = (params && params.autoStart !== null) || true;
+		var batchSize = (params && params.batchSize) || 1;
+		var interval = (params && params.interval) || 1;
+		var workArgs = (params && params.workArgs) || [];
+		var workFn = workFn;
+		var threadID = null;
 
-	var log = function(msg) {
-		console.log("SimpleThread #" + threadID + ": " + msg);
-	}
+		var log = function(msg) {
+			console.log("SimpleThread #" + threadID + ": " + msg);
+		}
 
-	// Control functions
-	this.start = function() {
-		this.stop();
-		var fn = function() {
-			for (var i = 0; i < batchSize; i++) {
-				if (!workFn.apply(window,workArgs)) {
-					that.stop();
-					break;
+		// Control functions
+		this.start = function() {
+			this.stop();
+			var go = function() { threadID = setTimeout(fn,interval); };
+			var fn = function() {
+				var keepGoing = true;
+				for (var i = 0; i < batchSize; i++) {
+					if (!workFn.apply(window,workArgs)) {
+						stop();
+						keepGoing = false;
+						break;
+					}
 				}
+				if (keepGoing) {
+					go();
+				}
+			};
+			go();
+			log("started");
+		};
+		this.stop = function() {
+			if (this.isRunning()) {
+				log("stopped");
+				clearTimeout(threadID);
+				threadID = null;
 			}
 		};
-		threadID = setInterval(fn,interval);
-		log("started");
-		return threadID;
-	};
-	this.stop = function() {
-		if (this.isRunning()) {
-			log("stopped");
-			clearInterval(threadID);
-			threadID = null;
+		this.isRunning = function() {
+			return threadID !== null;
+		}
+
+		// Getters/Setters
+		this.getBatchSize	= function()	{ return batchSize; };
+		this.setBatchSize	= function(n)	{ batchSize = n; };
+		this.getInterval	= function()	{ return interval; if (that.isRunning()) { that.stop; that.start();} };
+		this.setInterval	= function(n)	{ interval = n; };
+		this.getWorkFn		= function() 	{ return workFn; };
+		this.setWorkFn		= function(fn)	{ workFn = fn; };
+		this.getWorkArgs	= function()	{ return workArgs; };
+		this.setWorkArgs	= function(args){ workArgs = args; };
+		this.getThreadID	= function()	{ return threadID; };
+
+		if (autoStart) {
+			this.start();
 		}
 	};
-	this.isRunning = function() {
-		return threadID !== null;
-	}
-
-	// Getters/Setters
-	this.getBatchSize	= function()	{ return batchSize; };
-	this.setBatchSize	= function(n)	{ batchSize = n; };
-	this.getInterval	= function()	{ return interval; if (that.isRunning()) { that.stop; that.start();} };
-	this.setInterval	= function(n)	{ interval = n; };
-	this.getWorkFn		= function() 	{ return workFn; };
-	this.setWorkFn		= function(fn)	{ workFn = fn; };
-	this.getWorkArgs	= function()	{ return workArgs; };
-	this.setWorkArgs	= function(args){ workArgs = args; };
-	this.getThreadID	= function()	{ return threadID; };
-
-	if (autoStart) {
-		this.start();
-	}
-};
+})();
 
--- st.js
+++ st.js
@@ -1,6 +1,4 @@
 var itemCount = 100;
-var batchSize = 5;
-var singlelimit = 1000;
 var qs = window.location.search.split('?');
 if (qs.length > 1) {
 	qs = qs[1].split('&');
@@ -9,48 +7,19 @@ if (qs.length > 1) {
 		if (x.length > 1 && x[0] == 'n') {
 			itemCount = parseInt(x[1]);
 		}
-		if (x.length > 1 && x[0] == 's') {
-			batchSize = parseInt(x[1]);
-		}
 	}
 }
 
-var functions = [
+var jQFunctions = [
 	function(n, context) {
-		$(context).append($("<div/>").addClass("result").attr("id","r"+n).text(n));
+		$(context).append($("<div/>").addClass("result").attr("id","jqr"+n).text(n));
 	},function(n, context) {
-		$("#r" + n + ".result", context).addClass("dark");
+		$("#jqr" + n + ".result", context).addClass("dark");
 	},function(n, context){
-		$("#r" + n + ".result", context).remove();
+		$("#jqr" + n + ".result", context).remove();
 	}];
-
-/** singlethreaded approach **/
-var singleStage = 0;
-var SingleGenerator = function(n) {
-	var i = 0;
-	this.next = function() {
-		if (i > n || i > singlelimit) {
-			return null;
-		}
-		return i++;
-	};
-};
-
-var startsingle = function() {
-	var context = $("#singlethreaded .output");
-	var gen = new SingleGenerator(itemCount);
-	var n;
-	var workFn = functions[singleStage];
-	singleStage++;
-	singleStage = singleStage % functions.length;
-	while ((n = gen.next()) !== null) {
-		workFn(n,context);
-	}
-};
-
-/** simplethreaded approach **/
-var simpleStage = 0;
-var SimpleGenerator = function(n) {
+var jQStage = 0;
+var JQGenerator = function(n) {
 	var i = 0;
 	this.next = function() {
 		if (i > n) {
@@ -59,13 +28,12 @@ var SimpleGenerator = function(n) {
 		return i++;
 	};
 };
-
-var startsimple = function() {
-	var context = $("#simplethreaded .output");
-	var gen = new SimpleGenerator(itemCount);
-	var workFn = functions[simpleStage];
-	simpleStage++;
-	simpleStage = simpleStage % functions.length;
+var startjQuery = function() {
+	var context = $("#jquery-selectors .output");
+	var gen = new JQGenerator(itemCount);
+	var workFn = jQFunctions[jQStage];
+	jQStage++;
+	jQStage = jQStage % jQFunctions.length;
 	var fn = function() {
 		var n = gen.next()
 		if (n !== null) {
@@ -73,17 +41,57 @@ var startsimple = function() {
 		} else {
 			return false;
 		}
-		thread.setInterval(thread.getInterval() + 100);
 		return true;
 	};
 	var thread = new SimpleThread(fn,{
-		workArgs:[gen,context],
-		batchSize:batchSize});
+		workArgs:[gen,context]});
 };
 
+var domFunctions = [
+	function(n, root) {
+		var div = document.createElement("div");
+		div.appendChild(document.createTextNode(n));
+		div.className = 'result';
+		div.class = 'result';
+		div.id = 'domr' + n;
+		root.appendChild(div);
+	},function(n, context) {
+		document.getElementById('domr' + n).className = 'result dark';
+	},function(n, context){
+		$("#domr" + n + ".result", context).remove();
+	}];
+var domStage = 0;
+var DomGenerator = function(n) {
+	var i = 0;
+	this.next = function() {
+		if (i > n) {
+			return null;
+		}
+		return i++;
+	};
+};
+var startDom = function() {
+	var root = $("#pure-dom .output").get(0);
+	var gen = new DomGenerator(itemCount);
+	var workFn = domFunctions[domStage];
+	domStage++;
+	domStage = domStage % domFunctions.length;
+	var fn = function() {
+		var n = gen.next()
+		if (n !== null) {
+			workFn(n,root);
+		} else {
+			return false;
+		}
+		return true;
+	};
+	var thread = new SimpleThread(fn,{
+		workArgs:[gen,root],
+		batchSize:500});
+};
 
 $(function(){
-	$("#singlethreaded .start").bind('click', startsingle);
-	$("#simplethreaded .start").bind('click', startsimple);
+	$("#jquery-selectors .start").bind('click', startjQuery);
+	$("#pure-dom .start").bind('click', startDom);
 });
 
