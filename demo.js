b4835903c3a0fea30f0c1d8e91a28401d81a2f8c
Ben Cherry <bcherry@gmail.com>
2009-09-17 00:26:15 -0700

    finished demo modifications

---
+++ demo.js
@@ -0,0 +1,66 @@
+var startAnimation = function() {
+	var root = $("#basic-animation .output");
+	var div = $("<div/>").css({
+		"width":"220px",
+		"height":"50px",
+		"margin":"5px auto",
+		"position":"relative"
+	});
+	var target = $("<div/>").css({
+		"width":"50px",
+		"height":"50px",
+		"background-color":"#666666",
+		"position":"absolute",
+		"left":"0",
+		"top":"0",
+		"cursor":"pointer"
+	}).click(function(){target.parent().remove();thread.stop();});
+	div.append(target);
+	root.append(div);
+	var delta = 5;
+	var fn = function() {
+		var left = target.position().left;
+		if (left > 170 || left < 0) {
+			delta = -delta;
+		}
+		target.css("left", left + delta);
+	};
+	var thread = new SimpleThread(fn);
+};
+$("#basic-animation .start").click(startAnimation);
+
+(function() {
+	var thread;
+	var root = $("#element-manipulation .output");
+	var start = function() {
+		if (thread && thread.isRunning()) {
+			return false;
+		}
+		var i = 0;
+		var buildFn = function() {
+			root.append($("<div/>").attr("id","r" + i).addClass("result").text(i));
+			i++;
+			if (i >= 100) {
+				i = 0;
+				thread.setWorkFn(colorFn);
+			}
+		};
+		var colorFn = function() {
+			$("#r" + i, root).addClass("dark");
+			i++;
+			if (i >= 100) {
+				i = 0;
+				thread.setWorkFn(deleteFn);
+			}
+		};
+		var deleteFn = function() {
+			$("#r" + i, root).remove();
+			i++;
+			if (i >= 100) {
+				return false;
+			}
+		};
+		thread = new SimpleThread(buildFn);
+	};
+	$("#element-manipulation .start").click(start);
+})();
